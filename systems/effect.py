12d48781253bf5c7848b32901be3b3bc4517bdd3
Tim Lamborn <tlamborn@gmail.com>
2024-08-17 15:49:33 +1200

    Effect templates

--- systems/effect.py
+++ systems/effect.py
@@ -13,7 +13,7 @@ Component class to store effect information
 @enumerate_component("effect")
 class EffectComponent():
     direction: Vector2 = factory(Vector2)
-    energy: int = 0
+    energy: int = 1
     is_wave: bool = True
 
 
@@ -46,20 +46,50 @@ def effect_update_system(group: EntityGroup):
         effect.energy -= 1
         if effect.energy <= 0:
             group.remove(e)
-        
 
 
+'''
+Propagates an existing effect, inheriting some energy
+'''
 def propagate_entity(e: Entity, position: Vector2, energy: int) -> Entity:
     e = e.clone()
-    e.motion = MotionComponent(position=position)
+    e.motion.position = position
     e.effect.energy = energy
     return e
 
-def create_effect(position: Vector2, energy: int = 3) -> Entity:
-    e = Entity("effect")
-    e.motion = MotionComponent(position=Vector2(position))
+
+'''
+Instantiate all the template effects. These can be cloned when required
+'''
+def create_effect_templates():
+    effect_dict = {}
+
+    e = Entity("effect-fire")
+    e.motion = MotionComponent()
     e.sprite = SpriteComponent.from_circle(16, (255,0,0))
-    e.effect = EffectComponent(direction=Vector2(1,0),energy=energy)
+    e.effect = EffectComponent()
+    e.effect.energy = 3
+    effect_dict["fire"] = e
+
+    e = Entity("effect-wave")
+    e.motion = MotionComponent()
+    e.sprite = SpriteComponent.from_circle(16, (0,0,255))
+    e.effect = EffectComponent()
+    e.effect.energy = 3
+    effect_dict["wave"] = e
+
+    return effect_dict
+
+EFFECT_TEMPLATES = create_effect_templates()
+
+
+'''
+Spawns a new effect (as per a spell)
+'''
+def create_effect(type: str, position: Vector2, direction: Vector2 = Vector2(0)) -> Entity:
+    e = EFFECT_TEMPLATES[type].clone()
+    e.motion.position = Vector2(position)
+    e.effect.direction = direction
     return e
 
 
@@ -69,4 +99,4 @@ Mounts the effect updating system
 def mount_effect_system(group: EntityGroup):
     group.mount_system(effect_update_system)
 
-    group.add( create_effect(Vector2(3,3)) )
+    group.add( create_effect("wave", Vector2(3,3), Vector2(1,0) ) )
