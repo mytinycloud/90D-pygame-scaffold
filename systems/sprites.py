4b153cd1ec74fdfa1185518674a4bee8a8c9aa14
Sam <hoppelite@gmail.com>
2024-08-20 22:01:54 +1200

    Cache scaled tile sprites

--- systems/sprites.py
+++ systems/sprites.py
@@ -75,13 +75,14 @@ def draw_sprite_system(group: EntityGroup):
     sprite_scale = scale / TILE_SCALE
 
     asset_pipeline = AssetPipeline.get_instance()
+    scaled_sprites = {key: pygame.transform.scale_by(sprite, sprite_scale) for key, sprite in TILE_SPRITES.items()}
+    unknown_sprite = pygame.transform.scale_by(asset_pipeline.get_image('tiles/unknown.png'), sprite_scale)
 
     tile_center = Vector2(scale / 2)
     for y in range(tilemap.bounds.left, tilemap.bounds.right):
         for x in range(tilemap.bounds.top, tilemap.bounds.bottom):
             tile = tilemap.get_tile((x,y))
-            tile_surface = TILE_SPRITES.get(tile) or asset_pipeline.get_image('tiles/unknown.png')
-            scaled_sprite = pygame.transform.scale_by(tile_surface, sprite_scale)
+            scaled_sprite = scaled_sprites.get(tile) or unknown_sprite
             screen_pos = Vector2(x,y) * scale + offset
             surface.blit(scaled_sprite, screen_pos - tile_center)
 
